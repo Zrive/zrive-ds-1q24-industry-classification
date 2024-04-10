78691beec76a557d7915a4a98987feda45b61565
eduardodelreal <pablo.e.delrealbua@gmail.com>
2024-04-10 18:11:35 +0200

    LISTO PARA PR

--- src/models/df_coverwallet_creation.py
+++ src/models/df_coverwallet_creation.py
@@ -6,32 +6,41 @@ import matplotlib.pyplot as plt
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-route1 = '../data/SBS_Certified_Business_List.csv'   
+class DataProcessor:
+    def __init__(self, route_certified_business, route_sba_dataset):
+        self.route_certified_business = route_certified_business
+        self.route_sba_dataset = route_sba_dataset
 
-route2 = '../data/sba_dataset.csv'
+    def load_and_process(self):
+        try:
+            df_certified_business = pd.read_csv(self.route_certified_business)
+            df_sba_dataset = pd.read_csv(self.route_sba_dataset)
+        except Exception as e:
+            logging.error("Error loading csv: %s", e)
+            return None
 
-try:  
-    df1 = pd.read_csv(route1)
-    df2 = pd.read_csv(route2)
-except Exception as e:
-    logging.error("Error loading csv: %s", e)
+        df_certified_business_filtered = self._process_df1(df_certified_business)
+        df_sba_dataset = self._process_df2(df_sba_dataset)
 
-#df1
-df1_filtered = df1.dropna(subset=['Business_Description'])[['ID6_digit_NAICS_code', 'Business_Description']]
-df1_filtered.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
-df1_filtered['NAICS'] = df1_filtered['NAICS'].astype(str)
+        df_concatenated = pd.concat([df_certified_business_filtered, df_sba_dataset], ignore_index=True)
+        final_df = self._finalize_dataframe(df_concatenated)
 
-#df2
-df4_filtered = df2[df2['NAICS'].apply(lambda x: ',' not in str(x) and ' ' not in str(x))][['NAICS', 'DESCRIPTION_OF_OPERATIONS']]
-df4_filtered.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
-df4_filtered['NAICS'] = df4_filtered['NAICS'].astype(str)
+        return final_df
 
-#concatenation
-df_concatenated = pd.concat([df1_filtered, df4_filtered], ignore_index=True)
+    def _process_df1(self, df):
+        df_filtered = df.dropna(subset=['Business_Description'])[['ID6_digit_NAICS_code', 'Business_Description']]
+        df_filtered.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
+        df_filtered['NAICS'] = df_filtered['NAICS'].astype(str)
+        return df_filtered
 
-final_df = df_concatenated.dropna(subset=['NAICS', 'BUSINESS_DESCRIPTION'])
-final_df = final_df[['NAICS', 'BUSINESS_DESCRIPTION']]
-final_df.dropna()
+    def _process_df2(self, df):
+        df_filtered = df[df['NAICS'].apply(lambda x: ',' not in str(x) and ' ' not in str(x))][['NAICS', 'DESCRIPTION_OF_OPERATIONS']]
+        df_filtered.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
+        df_filtered['NAICS'] = df_filtered['NAICS'].astype(str)
+        return df_filtered
 
-print(final_df.head())
-
+    def _finalize_dataframe(self, df):
+        final_df = df.dropna(subset=['NAICS', 'BUSINESS_DESCRIPTION'])
+        final_df = final_df[['NAICS', 'BUSINESS_DESCRIPTION']]
+        final_df.dropna()
+        return final_df
