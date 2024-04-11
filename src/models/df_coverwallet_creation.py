0708fb4b053ea018d74853a787798ce611bc60fd
eduardodelreal <pablo.e.delrealbua@gmail.com>
2024-04-11 22:41:16 +0200

    final changes

--- src/models/df_coverwallet_creation.py
+++ src/models/df_coverwallet_creation.py
@@ -20,29 +20,28 @@ class DataProcessor:
             logging.error("Error loading csv: %s", e)
             return None
 
-        df_certified_business_filtered: pd.DataFrame = self._process_df1(df_certified_business)
-        df_sba_dataset: pd.DataFrame = self._process_df2(df_sba_dataset)
+        df_certified_business: pd.DataFrame = self.process_certified_business(df_certified_business)
+        df_sba_dataset: pd.DataFrame = self.process_sba(df_sba_dataset)
 
-        df_concatenated: pd.DataFrame = pd.concat([df_certified_business_filtered, df_sba_dataset], ignore_index=True)
-        final_df: pd.DataFrame = self._finalize_dataframe(df_concatenated)
+        jointNAICSBusinessDatasetDF: pd.DataFrame = pd.concat([df_certified_business, df_sba_dataset], ignore_index=True)
+        unifiedNAICSDescriptionsDF: pd.DataFrame = self._finalize_dataframe(jointNAICSBusinessDatasetDF)
+        return unifiedNAICSDescriptionsDF
 
-        return final_df
+    def process_certified_business(self, df: pd.DataFrame) -> pd.DataFrame:
+        filteredNAICSBusinessDF_certified_business: pd.DataFrame = df.dropna(subset=['Business_Description'])[['ID6_digit_NAICS_code', 'Business_Description']]
+        filteredNAICSBusinessDF_certified_business.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
+        filteredNAICSBusinessDF_certified_business['NAICS'] = filteredNAICSBusinessDF_certified_business['NAICS'].astype(str)
+        return filteredNAICSBusinessDF_certified_business
 
-    def _process_df1(self, df: pd.DataFrame) -> pd.DataFrame:
-        df_filtered: pd.DataFrame = df.dropna(subset=['Business_Description'])[['ID6_digit_NAICS_code', 'Business_Description']]
-        df_filtered.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
-        df_filtered['NAICS'] = df_filtered['NAICS'].astype(str)
-        return df_filtered
-
-    def _process_df2(self, df: pd.DataFrame) -> pd.DataFrame:
-        df_filtered: pd.DataFrame = df[df['NAICS'].apply(lambda x: ',' not in str(x) and ' ' not in str(x))][['NAICS', 'DESCRIPTION_OF_OPERATIONS']]
-        df_filtered.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
-        df_filtered['NAICS'] = df_filtered['NAICS'].astype(str)
-        return df_filtered
+    def process_sba(self, df: pd.DataFrame) -> pd.DataFrame:
+        filteredNAICSBusinessDF_sba_dataset: pd.DataFrame = df[df['NAICS'].apply(lambda x: ',' not in str(x) and ' ' not in str(x))][['NAICS', 'DESCRIPTION_OF_OPERATIONS']]
+        filteredNAICSBusinessDF_sba_dataset.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
+        filteredNAICSBusinessDF_sba_dataset['NAICS'] = filteredNAICSBusinessDF_sba_dataset['NAICS'].astype(str)
+        return filteredNAICSBusinessDF_sba_dataset
 
     def _finalize_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
-        final_df: pd.DataFrame = df.dropna(subset=['NAICS', 'BUSINESS_DESCRIPTION'])
-        final_df = final_df[['NAICS', 'BUSINESS_DESCRIPTION']]
-        final_df.dropna()
-        return final_df
+        finalMergedNAICSDataFrame: pd.DataFrame = df.dropna(subset=['NAICS', 'BUSINESS_DESCRIPTION'])
+        finalMergedNAICSDataFrame = finalMergedNAICSDataFrame[['NAICS', 'BUSINESS_DESCRIPTION']]
+        finalMergedNAICSDataFrame.dropna()
+        return finalMergedNAICSDataFrame
 
