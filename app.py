e1c89ff4f375fa91062edb78f165ac2cb5e5ec44
eduardodelreal <pablo.e.delrealbua@gmail.com>
2024-04-24 22:43:05 +0200

    before merge

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import logging
 from src.models.df_coverwallet_creation import DataProcessor
-from src.models.data_aumentation_2_digits import NAICSRebalancer
+from src.models.data_augmentation import NAICSRebalancer
 import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
-    '''
-    ROUTE_CERTIFIED_BUSSINESS = 'src/data/SBS_Certified_Business_List.csv'   
-    ROUTE_SBA_DATASET = 'src/data/sba_dataset.csv'
-
-    processor = DataProcessor(ROUTE_CERTIFIED_BUSSINESS, ROUTE_SBA_DATASET)
-    df_final = processor.load_and_process()
-
-    if df_final is not None:
-        logging.info("\n%s", df_final.head())
-    '''    
     
     ROUTE_COVERWALLET= 'src/data/coverwallet.xlsx'
     
