67513f206d27f83d5ea26f25686f7504a08a534b
eduardodelreal <pablo.e.delrealbua@gmail.com>
2024-04-24 11:37:13 +0200

    .

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from src.models.df_coverwallet_creation import DataProcessor
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
+    
+    '''
     ROUTE_CERTIFIED_BUSSINESS = 'src/data/SBS_Certified_Business_List.csv'   
     ROUTE_SBA_DATASET = 'src/data/sba_dataset.csv'
 
@@ -12,6 +14,8 @@ def main():
 
     if df_final is not None:
         logging.info("\n%s", df_final.head())
+    ''' 
+    
 
 if __name__ == '__main__':
     main()
