26cf9988932d4b3a3e78825f3347731d65fb61c2
eduardodelreal <pablo.e.delrealbua@gmail.com>
2024-04-25 19:49:55 +0200

    ready for PR

--- app.py
+++ app.py
@@ -6,31 +6,15 @@ import pandas as pd
 
 from src.models.classification_synthetic_no_cv import SummaryClassificationSyntheticNoCV
 import matplotlib.pyplot as plt
+from src.models.data_multilabel import DataProcessor
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def main():
+def main(): 
     
-
-    ROUTE_COVERWALLET= 'src/data/coverwallet.xlsx'
+    processor = DataProcessor()
+    processor.run()
     
-    df_coverwallet_aumentation = pd.read_excel(ROUTE_COVERWALLET)
-    rebalancer = NAICSRebalancer()
-    df_naics_rebalanced = rebalancer.rebalance_df_naics(df_coverwallet_aumentation)
-    logging.info(f"Number of rows before EDA: {len(df_coverwallet_aumentation)}")
-    logging.info(f"Number of rows after EDA: {len(df_naics_rebalanced)}")
-
-    if df_naics_rebalanced is not None: 
-        logging.info("\n%s", df_naics_rebalanced.head())   
-        
-
-
-    ROUTE_SYNTHETIC= 'src/data/synthetic_preprocessed.parquet' 
-    ROUTE_COVERWALLET= 'src/data/coverwallet.xlsx'
-    
-    summary_classifier = SummaryClassificationSyntheticNoCV(ROUTE_SYNTHETIC, ROUTE_COVERWALLET)
-    summary_classifier.run()
-
 if __name__ == '__main__':
     main()
 
--- src/models/data_multilabel.py
+++ src/models/data_multilabel.py
@@ -1,46 +1,52 @@
 import pandas as pd
 import logging
 import matplotlib.pyplot as plt
+from typing import List
+
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class DataProcessor:
     def __init__(self):
-        self.ROUTE_SBA = '../src/data/sba_dataset.csv'
-        self.ROUTE_SBS = '../src/data/SBS_Certified_Business_List.csv'
-        self.df_naics = None
-
-    def load_data(self):
-        df_sba = pd.read_csv(self.ROUTE_SBA)
-        df_sba = df_sba[['NAICS', 'DESCRIPTION_OF_OPERATIONS']]
-
+        self.ROUTE_SBA = 'src/data/sba_dataset.csv'
+        self.ROUTE_SBS = 'src/data/SBS_Certified_Business_List.csv'
+    
+    def load_data(self) -> None:
+        self.df_sba = pd.read_csv(self.ROUTE_SBA)[['NAICS', 'DESCRIPTION_OF_OPERATIONS']]
         df_sbs = pd.read_csv(self.ROUTE_SBS)
-        df_certified_business_processed = df_sbs.dropna(subset=['Business_Description'])[['ID6_digit_NAICS_code', 'Business_Description']]
-        df_certified_business_processed.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
-        df_certified_business_processed['NAICS'] = df_certified_business_processed['NAICS'].astype(str)
-        df_certified_business_processed['NAICS'] = df_certified_business_processed['NAICS'].apply(lambda x: x.split(',') if isinstance(x, str) else [x])
+        df_certified = df_sbs.dropna(subset=['Business_Description'])[['ID6_digit_NAICS_code', 'Business_Description']]
+        df_certified.columns = ['NAICS', 'BUSINESS_DESCRIPTION']
+        df_certified['NAICS'] = df_certified['NAICS'].astype(str)
+        df_certified['NAICS'] = df_certified['NAICS'].apply(lambda x: x.split(',') if isinstance(x, str) else [x])
 
-        df_clean_sba = df_sba.dropna()
+        df_clean_sba = self.df_sba.dropna()
         df_clean_sba['NAICS'] = df_clean_sba['NAICS'].apply(lambda x: x.split(',') if isinstance(x, str) else x)
         df_clean_sba.rename(columns={'DESCRIPTION_OF_OPERATIONS': 'BUSINESS_DESCRIPTION'}, inplace=True)
-        self.df_naics = pd.concat([df_certified_business_processed, df_clean_sba], ignore_index=True)
+        self.df_naics = pd.concat([df_certified, df_clean_sba], ignore_index=True)
 
-    def truncate_naics_codes(self, digits: int):
+    def truncate_naics_codes(self, digits: int) -> None:
         if 'NAICS' in self.df_naics.columns:
             self.df_naics['NAICS'] = self.df_naics['NAICS'].apply(
                 lambda naics_list: [str(code).strip()[:digits] for code in naics_list]
             )
 
-    def count_unique_naics(self):
+    def count_unique_naics(self) -> pd.Series:
         if 'NAICS' not in self.df_naics.columns:
-            logging.info("La columna 'NAICS' no está presente en el DataFrame.")
-            return
+            logging.error("La columna 'NAICS' no está presente en el DataFrame.")
+            return pd.Series()
         
         flattened_naics = self.df_naics['NAICS'].explode()
-        naics_counts = flattened_naics.value_counts()
-        return naics_counts
+        return flattened_naics.value_counts()
 
-    def plot_naics_distribution(self):
+    def count_distinct_naics(self) -> int:
+        if 'NAICS' not in self.df_naics.columns:
+            logging.error("La columna 'NAICS' no está presente en el DataFrame.")
+            return 0
+        
+        flattened_naics = self.df_naics['NAICS'].explode()
+        return flattened_naics.nunique()
+
+    def plot_naics_distribution(self) -> None:
         expanded_naics = self.df_naics['NAICS'].explode()
         naics_counts = expanded_naics.value_counts()
         naics_counts_df = pd.DataFrame({'NAICS': naics_counts.index, 'Count': naics_counts.values})
@@ -54,9 +60,24 @@ class DataProcessor:
         plt.tight_layout()
         plt.show()
 
-    def run_all(self):
+    def one_hot_encode_naics(self) -> pd.DataFrame:
+        expanded_naics = self.df_naics['NAICS'].explode().reset_index()
+        one_hot_encoded = pd.get_dummies(expanded_naics, columns=['NAICS'])
+        df_naics_encoded = one_hot_encoded.groupby('index').sum()
+        df_naics_encoded.reset_index(drop=True, inplace=True)
+        df_naics_final = pd.concat([self.df_naics.drop('NAICS', axis=1), df_naics_encoded], axis=1)
+        df_naics_final.columns = [col.replace('NAICS_', '') if 'NAICS_' in col else col for col in df_naics_final.columns]
+        return df_naics_final
+
+    def run(self) -> None:
         self.load_data()
         self.truncate_naics_codes(2)
         unique_naics_count = self.count_unique_naics()
-        logging.info(f'\n{unique_naics_count.head()}')
+        distinct_naics = self.count_distinct_naics()
+        logging.info(f'Total unique NAICS codes: {distinct_naics}')
+        logging.info(f'\n{unique_naics_count}')
         self.plot_naics_distribution()
+        df_naics_final = self.one_hot_encode_naics()
+        logging.info(f'\n{df_naics_final.head()}')
+
+
